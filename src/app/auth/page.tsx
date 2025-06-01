43d9e8fff9926e2fe862907ed2a6fb5b9a904e7b
yoruwitch <evelyn.fernandes4@gmail.com>
2025-06-01 16:49:39 -0300

    chore: add header to the form

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,4 +1,5 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
+import Image from "next/image";
 
 import LogInForm from "./components/log-in-form";
 import SignUpForm from "./components/sign-up-form";
@@ -6,8 +7,20 @@ import SignUpForm from "./components/sign-up-form";
 const AuthenticationPage = () => {
   return (
     <div className="flex h-screen w-full items-center justify-center bg-gray-100">
+      <header className="mr-4 flex flex-col">
+        <div className="self-center">
+          <Image
+            src="/assets/logo-form.svg"
+            alt="Logo"
+            width={250}
+            height={250}
+          />
+        </div>
+        <h1 className="text-2xl font-bold">Manage your clinics right here</h1>
+        <p className="text-gray-600">Please log in or register to continue</p>
+      </header>
       <Tabs defaultValue="login" className="w-[400px]">
-        <TabsList className="grid w-full grid-cols-2">
+        <TabsList className="mb-4 grid w-full grid-cols-2">
           <TabsTrigger value="login">Login</TabsTrigger>
           <TabsTrigger value="register">Register</TabsTrigger>
         </TabsList>
